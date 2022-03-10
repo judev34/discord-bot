641251a1335f9db74559b9b220defe78de50a1a3
judev34 <julien.coduert34@gmail.com>
2022-03-10 14:05:29 +0100

    feat(randomNumber) : add command

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,4 +1,5 @@
 import { Command } from "./Command";
 import { Hello } from "./commands/Hello";
+import { RandomNumber } from "./commands/RandomNumber";
 
-export const Commands: Command[] = [Hello];
+export const Commands: Command[] = [Hello, RandomNumber];
