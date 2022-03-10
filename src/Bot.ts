40482c48415e49e710c7fbdcee1a244030f26a7d
judev34 <julien.coduert34@gmail.com>
2022-03-10 10:00:52 +0100

    feat(startproject) : correction Token

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,8 +2,6 @@ import { Client } from "discord.js";
 import ready from "./listeners/ready";
 
 
-const token = "OTUxMzk5MzczMTY4ODYxMjI1.Yim5tg.aDxB77zzMbqEiguAD2lvU6-5FZY"
-
 console.log("Bot is Starting");
 
 const client = new Client({
@@ -12,7 +10,7 @@ const client = new Client({
 
 ready(client);
 
-client.login(token);
+client.login(process.env.TOKEN);
 
 console.log(client);
 
