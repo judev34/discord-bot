ab7890a88151af12eaebc62c06e69bb135eaafe3
judev34 <julien.coduert34@gmail.com>
2022-03-11 11:02:48 +0100

    feat(newversion): finish test

--- src/index.js
+++ src/index.js
@@ -7,39 +7,15 @@ const { token } = require('../config.json');
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+for (const eventFile of eventFiles) {
+	const event = require(`./events/${eventFile}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(client, ...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(client, ...args));
+	}
 }
-// When the client is ready, run this code (only once)
-client.once('ready', client => {
-	console.log(`Ready! Logged in as ${client.user.tag}!}`);
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) {
-		return;
-	}
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) {
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'Something went wrong!', ephemeral: true });
-	}
-});
-
-
 // Login to Discord with your client's token
 client.login(token);
